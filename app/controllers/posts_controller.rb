affecf46091bf9344e22c916fec3d598db47757f
Enoch Ko <enoch232@gmail.com>
2016-07-21 10:41:10 -0700

    added index, new, create methods and templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      sleep 10
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +24,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
